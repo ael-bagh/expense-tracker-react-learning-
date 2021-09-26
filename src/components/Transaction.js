ab1f9a8d7a3023bae3beb8214fcd2227e845f3b8
ael-bagh <ael-bagh@student.1337.ma>
2021-09-26 17:13:25 +0100

    state and inputs

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,18 @@
+import { useState } from 'react'
+
 const Transaction = () => {
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState(0)
     return (
-        <div className='transaction'>
+        <form className='transaction'>
             <h1>Add transaction</h1>
             <label>Text</label>
-            <input placeholder='Enter text...' />
+            <input placeholder='Enter text...'  value={text} onChange={(e) => setText(e.target.value)}/>
             <label>Amount</label>
             <p>(negative - expense, positive - income)</p>
-            <input placeholder='Enter amount' />
+            <input placeholder='Enter amount' value={amount} onChange={(e) => setAmount(e.target.value)}/>
             <button type='submit' className='bg-purple-500 text-white font-bold w-full mt-2 p-4 rounded-md'>Add transaction</button>
-        </div>
+        </form>
     )
 }
 
